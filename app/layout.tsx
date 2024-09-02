ab752f6ef92b531e2ae9f96391eade5af741d93c
Ajay Rathod <ajay.rathod0801@gmail.com>
2024-09-02 19:03:04 +0530

    feat: Add board card component with overlay and footer
    This commit adds a new board card component to display individual boards in the dashboard. The board card includes an overlay for hover effects and a footer section to display additional information such as the board title, author name, and creation date. This component will be used to render each board in the dashboard.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
 import { ConvexClientProvider } from "@/providers/convex-client-providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,7 +10,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aero",
   description: "Aero - A simple board tool for your organization",
-  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+          <Toaster />
           {children}
         </ConvexClientProvider>
       </body>
