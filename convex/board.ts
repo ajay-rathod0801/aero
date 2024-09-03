f73577170418b383913064efa24a383315af4020
Ajay Rathod <ajay.rathod0801@gmail.com>
2024-09-03 15:51:18 +0530

    board actions (copy link, rename and delete) complete

--- convex/board.ts
+++ convex/board.ts
@@ -29,7 +29,7 @@ export const create = mutation({
         const randomImage = images[Math.floor(Math.random() * images.length)];
 
         console.log(randomImage, "TEST");
-        
+
 
         const board = ctx.db.insert("boards", {
             title: args.title,
@@ -37,6 +37,46 @@ export const create = mutation({
             authorId: identity.subject,
             authorName: identity.name!,
             imageUrl: randomImage,
+        });
+
+        return board;
+    },
+});
+
+export const remove = mutation({
+    args: { id: v.id("boards") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.delete(args.id);
+    },
+});
+
+export const update = mutation({
+    args: { id: v.id("boards"), title: v.string() },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const title = args.title.trim();
+
+        if (!title) {
+            throw new Error("Title is required");
+        }
+
+        if (title.length > 60) {
+            throw new Error("Title cannot be more than 60 characters long")
+        }
+
+        const board = await ctx.db.patch(args.id, {
+            title: args.title,
         });
 
         return board;
